cb129a3e4a2367dc64bef4b37fab672a0dc45285
Alex Chistyakov <alexclear@gmail.com>
2016-05-03 01:34:21 +0300

    Try to collect kstat metrics

--- main.go
+++ main.go
@@ -3,12 +3,32 @@ package main
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/siebenmann/go-kstat"
 	"net/http"
+	"time"
 )
 
-func main () {
+func collectARCstats() {
+	log.Debugf("Start collecting ARC stats")
+	token, err := kstat.Open()
+	if err != nil {
+		log.Fatalf("Open failure: %s", err)
+	}
+	for {
+		log.Debugf("Collecting...")
+		ks, err := token.Lookup("zfs", 0, "arcstats")
+		if err != nil {
+			log.Fatalf("lookup failure on %s:0:%s: %s", "zfs", "arcstats", err)
+		}
+		log.Debugf("Collected: %v", ks)
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func main() {
 	log.SetLevel(log.DebugLevel)
 	log.Debugf("Starting")
+	go collectARCstats()
 	http.Handle("/metrics", prometheus.Handler())
 	err := http.ListenAndServe("0.0.0.0:9102", nil)
 	if err != nil {
