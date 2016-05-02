4146b7ad92c8de4228ecd9a784d80d28faed96e4
Alex Chistyakov <alexclear@gmail.com>
2016-05-02 23:10:11 +0000

    Add linker flags

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ mkdir $GOPATH/bin/solaris
 GOOS=solaris
 export GOOS
 go get golang.org/x/sys/unix
-GOARCH=amd64 go build -o $GOPATH/bin/solaris/smartos_exporter github.com/ingrians/smartos_exporter
+GOARCH=amd64 go build -ldflags '-linkmode internal' -o $GOPATH/bin/solaris/smartos_exporter github.com/ingrians/smartos_exporter
